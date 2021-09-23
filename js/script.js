9fbb3a827f348403ed0e085e535e6b017e2ad347
Laurent LOUIS-THERESE <laurent@bcsirt.fr>
2021-09-23 17:18:22 +0200

    Mise à jour du code HTML et CSS -ajout du burger menu -ajout script js -ajout effet titres.

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let path01 = document.querySelector("path:first-of-type");
+// let path02 = document.querySelector("path:nth-child(2)");
+// let path03 = document.querySelector("path:nth-child(3)");
+// let path04 = document.querySelector("path:nth-child(4)");
+// let path05 = document.querySelector("path:nth-child(5)");
+let pathLength = path01.getTotalLength();
+console.log(pathLength);
+// document.querySelector(".output").innerHTML = ("Longueur totale")
